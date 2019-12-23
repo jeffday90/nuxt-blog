7ee59d1d397d783a897e3a5c06fc053bbb307d4d
Jeff Day <jeffrey.day00@gmail.com>
2019-12-23 14:56:31 -0800

    Developed a reusable component that takes in dynamic data ~for the general page structure.

--- pages/index.vue
+++ pages/index.vue
@@ -4,28 +4,34 @@
       <h1>Get the latest in tech news!</h1>
     </section>
     <section class="featured-posts">
-      <nuxt-link :to="'/posts/' + 1" class="post-preview">
-        <article>
-          <div class="post-thumbnail" style="background-image: url('https://www.biznews.com/wp-content/uploads/2018/05/generic_technology_imagery.jpg')"></div>
-          <div class="post-content">
-            <h1>Post Title</h1>
-            <p>Preview text</p>
-          </div>
-        </article>
-      </nuxt-link>
-      <nuxt-link :to="'/posts/' + 2" class="post-preview">
-        <article>
-          <div class="post-thumbnail" style="background-image: url('https://www.biznews.com/wp-content/uploads/2018/05/generic_technology_imagery.jpg')"></div>
-          <div class="post-content">
-            <h1>Post Title 2</h1>
-            <p>Preview text 2</p>
-          </div>
-        </article>
-      </nuxt-link>
+      <PostPreview 
+      id="1"
+      thumbnail="https://www.biznews.com/wp-content/uploads/2018/05/generic_technology_imagery.jpg"
+      title="Hello there"
+      previewText="this is my first post"/>
+      <PostPreview       
+      id="2"
+      thumbnail="https://www.biznews.com/wp-content/uploads/2018/05/generic_technology_imagery.jpg"
+      title="Hello there, the second time"
+      previewText="this is my second post"/>
+      <PostPreview 
+      id="3"
+      thumbnail="https://www.biznews.com/wp-content/uploads/2018/05/generic_technology_imagery.jpg"
+      title="Hi this is my third"
+      previewText="this is my third post"/>
     </section>
-
   </div>
 </template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  }
+}
+</script>
 
 <style scoped>
 .intro {
@@ -65,42 +71,6 @@
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-}
-
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
 }
 </style>
 
