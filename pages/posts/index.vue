27d9b3e9645da99515266ee5f9b77c6781c2b0bc
Jeff Day <jeffrey.day00@gmail.com>
2019-12-26 18:15:36 -0800

    Created a store in the nuxt app.

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -6,34 +6,44 @@
 
 <script>
 import PostList from '@/components/Posts/PostList'
+import store from '@/store/index.js'
 
 export default {
-  asyncData(context){
-      return new Promise ((resolve, reject) => {
-        resolve({
-        loadedPosts: [
-          {
-            id: '32423', 
-            title: 'new post',
-            previewText: 'preview text for a new post',
-            thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo_RbXp2Yd-pGCDVJPLPdP_VtkZaDNMvXw3a5ZyAm9x5vc9OpZ&s'
-          }, 
-          {
-            id: '2342', 
-            title: 'newer post',
-            previewText: 'preview text for a newer post',
-            thumbnail: 'https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fs3-ap-northeast-1.amazonaws.com%2Fpsh-ex-ftnikkei-3937bb4%2Fimages%2F0%2F9%2F0%2F3%2F21543090-1-eng-GB%2FHUAWEI%20TECH-USA_CHIP-CATCHUP20190704010111116_Data_2048x1152.jpg?source=nar-cms'
-          }, 
-        ]
-        });
-      })
-      .then(data => {return data})
-      .catch(err => {context.error(new Error)})
-      
-  },
   components: {
     PostList
+  },
+  asyncData(context){
+      return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+          resolve({
+            loadedPosts: [
+            {
+                id: '32423', 
+                title: 'new post',
+                previewText: 'preview text for a new post',
+                thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo_RbXp2Yd-pGCDVJPLPdP_VtkZaDNMvXw3a5ZyAm9x5vc9OpZ&s'
+            }, 
+            {
+                id: '2342', 
+                title: 'newer post',
+                previewText: 'preview text for a newer post',
+                thumbnail: 'https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fs3-ap-northeast-1.amazonaws.com%2Fpsh-ex-ftnikkei-3937bb4%2Fimages%2F0%2F9%2F0%2F3%2F21543090-1-eng-GB%2FHUAWEI%20TECH-USA_CHIP-CATCHUP20190704010111116_Data_2048x1152.jpg?source=nar-cms'
+            }, 
+            ]
+          });
+        }, 1000);
+      })
+      .then(data => {
+          return data
+        })
+      .catch(err => {
+          context.error(new Error)
+        })
+  },
+  created() {
+      this.$store.dispatch('setPosts', this.loadedPosts)
   }
+
 }
 </script>
 
